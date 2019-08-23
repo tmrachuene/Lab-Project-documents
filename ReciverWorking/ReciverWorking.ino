d45925d6d8393609a60d81c6190d91f9712d5564
Tumisho Rachuene <1314101@students.wits.ac.za>
2019-08-23 22:41:31 +0200

    WIFI code added

--- ReciverWorking/ReciverWorking.ino
+++ ReciverWorking/ReciverWorking.ino
@@ -1,23 +1,19 @@
-         /**********************Calibration parameters***************************************/
-#define Ro_Clean_Air_Factor_MQ3               (60)  // Is the ratio of sensor resistance in clean air and Ro
-                                                    // It is found from the datasheet
-#define Ro_Clean_Air_Factor_MQ5               (6.5)
- 
-#define Calibration_Sample                    (15) // The number of times the value of the sensor will be read during calibration
-#define Calibration_Interval                  (100)// The time interval between each sample during calibration in miliseconds
+#include <SoftwareSerial.h> 
 
-/*********************Load resistance of the sonsors*******************************/
-#define MQ3_RL                                (1.5) // The load resistance of MQ3 from the mq3 datasheet in kohms
-#define MQ5_RL                                (1.5) // The load resistance of MQ5 fronm its datasheet in kohms
+#include <Wire.h>
+int ledPin = 13; 
 
-/*******************Reading parameters during normal operation ********************/
-#define Read_Sample                           (10) // The number of time the sensor read during normal operation
-#define Read_Interval                         (40) // Time interval in between the reading in milliseconds
+// ThingSpeak Write API key, this can be found in your channel under API Keys and you'll want the "Write API Key". 
+String apiKey = "EY6ENAPVHLEINCE6";
 
-/******************The Ro for LPG(MQ5) and Smoke(MQ3) *****************************/
-float Ro_LPG                                  (2.42) ;// initialise to 9
-float Ro_CO                                   (0.57) ;// initialise to 9
 
+#define SSID "con" 
+#define PASS "karabeloma0"
+
+// connect pin 12 to TX of the ESP8266 (Yellow wire if you've been following the videos.)
+// connect pin 11 to RX of the ESP8266 (Blue wire if you've been following the videos.)
+SoftwareSerial SoftSer(12, 11); // Everywhere from here on when you see SoftSer, it is being used as a command
+// in the same way you'd use Serial.print() or Serial.begin() only over pins 12 and 11 instead of the USB port.
 /**********************Senosor Pin Definitions***************************************/
 String MQ3  ;                                   // This defines the analog PIN (A0) , which will be connected to MQ3 gas sensor
 String MQ5  ;                                   // This defines the analog PIN A1 , which is connected to MQ5 gas sensor
@@ -40,9 +36,14 @@ int bluePin_CO = 6;
 
 /******************Program Setup *************************************************/
 void setup() {
+
+    // Setting pin 13, the LED indicator, to output.
+  pinMode(ledPin, OUTPUT);
   
 // Setup code here, to run once:
   Serial.begin(38400) ;
+    // Begins software serial communication at a baud of 9600.
+  SoftSer.begin(38400); 
   
   
   Serial.println("CLEARSHEET"); // clears EXCEL sheet starting at row 1
@@ -52,8 +53,21 @@ void setup() {
 }
 
 void loop() {
-  
 
+ /* String join="AT+CWJAP=\""; // Join accesspoint
+  join+=SSID;
+  join+="\",\"";
+  join+=PASS;
+  join+="\"";
+  SoftSer.println(join);
+  delay(5000);*/
+  //-----------------------------------------------*
+
+
+  // blink LED on board 
+  digitalWrite(ledPin, HIGH);
+  delay(200);
+  digitalWrite(ledPin, LOW);
    //Serial.println( (String) "DATA,DATE,TIME," + findPPM_LPG_MQ5() + "," + findPPM_CO_MQ3() + ",AUTOSCROLL_20" );
    
          if( Serial.available() > 0 ){
@@ -67,7 +81,84 @@ void loop() {
              MQ3_ADC_VALUE = MQ3.toInt();
              MQ5_ADC_VALUE = MQ5.toInt();
              Temp_ADC_VALUE = Temp.toInt();
-             Hum_ADC_VALUE =  Hum.toInt();
+             Hum_ADC_VALUE =  Hum.toInt(); 
+  
+
+            float temp = MQ3_ADC_VALUE;
+            float temp2 = MQ5_ADC_VALUE;
+            float temp3 = Temp_ADC_VALUE;
+            float temp4 = Hum_ADC_VALUE;
+
+
+
+        
+
+          // convert float to string-------------*
+  char buf[16]; // buf array can hold up to 16 characters. 
+  String strTemp = dtostrf(temp, 4, 1, buf); // "buf" will be the actual name of the array and strTemp is being equalled to it as a string.
+    // convert float to string-------------*
+  char buf1[16]; // buf array can hold up to 16 characters. 
+  String strTemp2 = dtostrf(temp2, 4, 1, buf1); // "buf" will be the actual name of the array and strTemp is being equalled to it as a string.
+    // convert float to string-------------*
+  char buf2[16]; // buf array can hold up to 16 characters. 
+  String strTemp3 = dtostrf(temp3, 4, 1, buf2); // "buf" will be the actual name of the array and strTemp is being equalled to it as a string.
+    // convert float to string-------------*
+  char buf3[16]; // buf array can hold up to 16 characters. 
+  String strTemp4 = dtostrf(temp4, 4, 1, buf3); // "buf" will be the actual name of the array and strTemp is being equalled to it as a string.
+
+
+  Serial.println(strTemp); // Prints the recorded temperature to the serial print.
+  Serial.println(strTemp2); // Prints the recorded temperature to the serial print.
+  Serial.println(strTemp3); // Prints the recorded temperature to the serial print.
+  Serial.println(strTemp4); // Prints the recorded temperature to the serial print.
+
+// TCP connection
+  String cmd = "AT+CIPSTART=\"TCP\",\"";
+  cmd += "184.106.153.149"; // api.thingspeak.com
+  cmd += "\",80";
+  SoftSer.println(cmd);
+
+  if (SoftSer.find("Error")) 
+  {
+    Serial.println("AT+CIPSTART error");
+    return;
+  }
+
+  // Putting together the GET string, this command structure can be found on your
+  // ThingSpeak channel in Data Import/Export, under "Update Channel Feed - GET". 
+  String getStr = "GET /update?api_key=";
+  getStr += apiKey;
+  getStr += "&field1=";
+  getStr += String(strTemp);
+  getStr += "&field2=";
+  getStr += String(strTemp2);
+  getStr += "&field3=";
+  getStr += String(strTemp3);
+  getStr += "&field4=";
+  getStr += String(strTemp4);
+  getStr += "\r\n\r\n";
+
+  // send data length
+  cmd = "AT+CIPSEND=";
+  cmd += String(getStr.length());
+  SoftSer.println(cmd);
+
+  if (SoftSer.find(">")) 
+  {
+    SoftSer.print(getStr); // Send data.
+  }
+  else 
+  {
+    SoftSer.println("AT+CIPCLOSE");
+    
+    Serial.println("AT+CIPCLOSE"); // If this shows on the serial monitor the data did not send.
+  }
+  // The AT+RST command resets the ESP8266, I'm doing this because data is uploaded much more reliably. 
+  SoftSer.println("AT+RST");
+  // ThingSpeak needs on average, a 15 second delay between updates. Change this to whatever suits your application
+  // e.g: delay(600000) is ten minutes.
+ 
+  
 
 //Threshold processes for air quality color system 
 if( MQ3_ADC_VALUE> 100){
@@ -110,13 +201,12 @@ if( MQ3_ADC_VALUE> 100){
 }
    Serial.println( (String) "DATA,DATE,TIME," + MQ3_ADC_VALUE+ "," + MQ5_ADC_VALUE +  "," + Temp_ADC_VALUE + "," + Hum_ADC_VALUE +  ",AUTOSCROLL_20" );
 
+
+ delay(1000);
 }
 
 
     
-
-
-
 
 void setColor(int red, int green, int blue)
 {
--- Transmitter-Reciever-code/analog_transmitString_final/analog_transmitString_final.ino
+++ Transmitter-Reciever-code/analog_transmitString_final/analog_transmitString_final.ino
@@ -2,9 +2,29 @@
 #include <dht11.h>
 #define DHT11PIN 4
 
+/**********************Senosor Pin Definitions***************************************/
+#define MQ3_PIN                               (0) // This define the analog PIN (A0) , which will be connected to MQ3 gas sensor
+#define MQ5_PIN                               (1) // This define the analog PIN A1 , which is connected to MQ5 gas sensor
+
+/**********************Calibration parameters***************************************/
+#define Ro_Clean_Air_Factor_MQ3               (60)  // Is the ratio of sensor resistance in clean air and Ro
+                                                    // It is found from the datasheet
+#define Ro_Clean_Air_Factor_MQ5               (6.5)
+ 
+#define Calibration_Sample                    (3) // The number of times the value of the sensor will be read during calibration
+#define Calibration_Interval                  (500) // The time interval between each sample during calibration in miliseconds
+
 /*********************Load resistance of the sonsors*******************************/
 #define MQ3_RL                                (1.5) // The load resistance of MQ3 from the mq3 datasheet in kohms
 #define MQ5_RL                                (1.5) // The load resistance of MQ5 fronm its datasheet in kohms
+
+/*******************Reading parameters during normal operation ********************/
+#define Read_Sample                           (25) // The number of time the sensor read during normal operation
+#define Read_Interval                         (10) // Time interval in between the reading in milliseconds
+
+/******************The Ro for LPG(MQ5) and Smoke(MQ3) *****************************/
+float Ro_LPG                                  (2.42) ;// initialise to 9
+float Ro_Smoke                                (0.57) ; // initialise to 9                                  (0.57) ;// initialise to 9
 
 /*******************RGB parameters**********************************************/
 int RED_PIN_CO = 10;
@@ -13,26 +33,37 @@ int GREEN_PIN_CO = 9;
 /*******************RGB parameters**********************************************/
 int RED_PIN_LPG =   5 ;
 int GREEN_PIN_LPG = 6 ;
-
-/*******************Sensor pins declaration ************************************/
-int CO_SENSOR_PIN  = A0; 
-int LPG_SENSOR_PIN = A1;   
-
-int CO_ANALOG_VALUE   = 0;
-int LPG_ANALOG_VALUE  = 0;
+ 
 
 int TEM_VALUE       =  0 ;
 int HUMIDITY_VALUE  =  0 ;
 
 dht11 DHT11;
 
-
-
 bool flag=false;
+
+int  MQ5_ppm = 0 ;
+int  MQ3_ppm = 0 ;
+
 void setup() {
 
   // Default communication rate of the Bluetooth module
   Serial.begin(38400);
+
+  //Calibrate the sensors to obtain Ro
+  Ro_LPG = sensorCalibration(MQ5_PIN ,  MQ5_RL ,Ro_Clean_Air_Factor_MQ5  );
+  Ro_Smoke = sensorCalibration( MQ3_PIN ,  MQ3_RL , Ro_Clean_Air_Factor_MQ3  ) ;
+
+
+  Serial.print("Calibration completed") ;
+  Serial.print('\n');
+  Serial.print("Ro_LPG : ") ;
+  Serial.print('\n');
+  Serial.print(Ro_LPG ) ;
+  Serial.print("\n") ;
+  Serial.print("Ro_SMOKE : ") ;
+  Serial.print(Ro_Smoke) ;
+  Serial.print("\n");
   
   pinMode(RED_PIN_CO, OUTPUT);
   pinMode(GREEN_PIN_CO , OUTPUT);
@@ -42,39 +73,41 @@ void setup() {
 
 void loop() {
   
- CO_ANALOG_VALUE  = analogRead(CO_SENSOR_PIN);
- LPG_ANALOG_VALUE = analogRead(LPG_SENSOR_PIN);
+ /*############################################################################*/
 
- int chk = DHT11.read(DHT11PIN);
+   int chk = DHT11.read(DHT11PIN);
  
- TEM_VALUE       =  DHT11.humidity ;
- HUMIDITY_VALUE  =  DHT11.temperature ;
+   TEM_VALUE       =  DHT11.humidity ;
+   HUMIDITY_VALUE  =  DHT11.temperature ;
 
- 
+   MQ5_ppm = findPPM_LPG_MQ5( ) ;
+   MQ3_ppm = findPPM_SMOKE_MQ3();
+
+/*############################################################################*/
 // write high to digital pin 10  when smoke level goes above 60(750ppm)
- if(CO_ANALOG_VALUE  > 131 ){
+ if(MQ3_ppm  > 100 ){
     RGB_Color(RED_PIN_CO ,GREEN_PIN_CO ,0 , 255 ) ;
     
   
-  }else if (CO_ANALOG_VALUE  >107 && CO_ANALOG_VALUE  < 131){
+  }else if (MQ3_ppm  >70 && MQ3_ppm  < 100){
      RGB_Color(RED_PIN_CO ,GREEN_PIN_CO ,5 , 215 ) ; 
     
-  }else if (CO_ANALOG_VALUE  >87 && CO_ANALOG_VALUE  < 107){
+  }else if (MQ3_ppm  >40 && MQ3_ppm  < 70){
      RGB_Color(RED_PIN_CO ,GREEN_PIN_CO ,0 , 0 ) ; 
     
   }else{
      RGB_Color(RED_PIN_CO ,GREEN_PIN_CO ,255 , 0 )  ;
     }
 
-
+/*############################################################################*/
 // write high to digital pin 12  when LPG level goes above 250(50ppm)
- if(LPG_ANALOG_VALUE  > 594 ){
+ if(MQ5_ppm  > 400 ){
   RGB_Color(RED_PIN_LPG ,GREEN_PIN_LPG  ,0 , 255 )  ;
   
-  }else if( LPG_ANALOG_VALUE >495 && LPG_ANALOG_VALUE < 594 ){
+  }else if( MQ5_ppm >300 && MQ5_ppm < 400 ){
     RGB_Color(RED_PIN_LPG ,GREEN_PIN_LPG ,5 , 215 ) ;
     
-  }else if( LPG_ANALOG_VALUE >395 && LPG_ANALOG_VALUE < 494 ){
+  }else if( MQ5_ppm >200 && MQ5_ppm < 300 ){
     RGB_Color(RED_PIN_LPG ,GREEN_PIN_LPG ,0 , 0 ) ;
     
   }else{
@@ -82,9 +115,9 @@ void loop() {
     
   }
   
-
- String str1 = String(CO_ANALOG_VALUE);
- String str2 = String(LPG_ANALOG_VALUE);
+/*############################################################################*/
+ String str1 = String(MQ3_ppm);
+ String str2 = String(MQ5_ppm);
 
  String str3 = String(TEM_VALUE );
  String str4 = String(HUMIDITY_VALUE);
@@ -105,11 +138,89 @@ for(int i=0;i<str_len;i++){
   } 
 }
 
+
+/*################################################################################################## */
 // This function calculates the sensor response(Rs) using given ADC value and Load resistance.
 float  findSensorResistance(int ADC_VALUE , int RL ){
      return((float) (RL*((1023 - ADC_VALUE)/ADC_VALUE ) )) ;
   }
 
+/*################################################################################################## */
+//This function  returns the Ro(The resistence of the sensor for clean air) of the sensor
+float  sensorCalibration(int MQ_PIN , float RL , float Ro_Clean_Air_Factor ){
+     float Rs = 0 ;
+     int index ;
+     
+     for(index =0 ; index < Calibration_Sample ; index++ ){
+        
+        Rs +=  findSensorResistance(analogRead( MQ_PIN) , RL ) ;
+        
+        delay( Calibration_Interval) ;
+      }
+  
+     Rs = Rs /Calibration_Sample ;
+ 
+     Rs = Rs/Ro_Clean_Air_Factor  ;
+     
+     return Rs  ;             // this value represent Rs in clean air which is Ro 
+  }
+
+/*################################################################################################## */
+// This function returns the adc value of sensor reading from analog pins
+float readSensorOutput( int MQ_PIN , int RL ){
+    float Rs;
+    
+    for(int index =0 ; index < Read_Sample  ; index++ ){
+       Rs +=   findSensorResistance( analogRead( MQ_PIN) , RL ) ;
+       delay(Read_Interval) ;
+    }
+
+    Rs = Rs/Read_Sample ;
+    return Rs ;                // return the average sensor rsistance or response given the ADC value
+
+  }
+
+
+/*################################################################################################## */
+  // This function calculate the PPM of  LPG gas given the Rs/Ro  
+ int findPPM_LPG_MQ5( ){
+
+  float RsRoRatio ;
+  RsRoRatio = readSensorOutput(MQ5_PIN ,  MQ5_RL) / Ro_LPG ;
+
+  float PPM_In_Log ;
+  float log_RsRoRatio = log10(RsRoRatio) ;
+  PPM_In_Log = (log_RsRoRatio *( - 2.5445 ))  + 1.879;  // The equation is found by doing regression of sensitity graph of MQ5 for LPG 
+                                                        // Mq5 and Excel are used to do the regression
+                                                  
+  float ppm ;
+  ppm = pow(10, PPM_In_Log  )  ;
+  // convert the log output to the normal value
+
+  return ppm ;
+    
+}
+
+
+/*################################################################################################## */
+// This function calculate the PPM of  Smoke   given the Rs/Ro  
+int findPPM_SMOKE_MQ3(){
+
+  float RsRoRatio ;
+  RsRoRatio = readSensorOutput(MQ3_PIN , MQ3_RL) / Ro_Smoke ;
+  
+  float PPM_In_Log  ;
+  float log_RsRoRatio = log10(RsRoRatio) ;
+ 
+  PPM_In_Log  = (log_RsRoRatio*( - 3.9651) )  + 8.6491 ;  // The equation is found by doing regression of sensitity graph of MQ3 for SMOKE(CO) 
+                                                          // Mq3 and Excel are used to do the regression
+  float ppm  ;
+  ppm = pow(10, PPM_In_Log  )  ;                          // convert the log output to the normal value
+  
+  return ppm ;  
+}
+
+/*################################################################################################## */
 // Is used to right PWM values to RGB pins
 void RGB_Color(int Red_pin , int Green_pin ,int red_light_value, int green_light_value)
  {
