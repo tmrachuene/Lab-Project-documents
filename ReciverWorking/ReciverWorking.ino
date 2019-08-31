cbc1174529821aed4e33c8269594b4dda2c5cc49
Tumisho Rachuene <1314101@students.wits.ac.za>
2019-08-31 20:40:18 +0200

    The receiver improved

--- ReciverWorking/ReciverWorking.ino
+++ ReciverWorking/ReciverWorking.ino
@@ -1,28 +1,33 @@
-
+// This code consists of my own work and others : James who developed temperature monitoring system code can be found on:
 /*************************WIFI SETUP*************************************/
 #include <SoftwareSerial.h>
 
+// define the SSID address and passoword for wifi connections
 #define SSID "LAPTOP-GQVDF9MH 6726"
 #define PASS "123456789#"
 
+// define the write key found on thingspeak IoT server
 String apiKey = "EY6ENAPVHLEINCE6";
 
-SoftwareSerial SoftSer(12, 11) ;
+// define the pins used to communicate with the esp8266  module
+SoftwareSerial Esp8266_Serial(12, 11) ;
 
+// define time interval to upload to thingspeak in milliseconds
 long startTime = 0 ;
 long waitTime = 0 ;
-long timeIntervalBetweenWrite = 5000 ;
+long timeIntervalBetweenWrite = 5000 ; // for this application 5 seconds interval is choosen .
 
 /**********************Senosor Pin Definitions***************************************/
 String MQ3  ;                                   // This defines the analog PIN (A0) , which will be connected to MQ3 gas sensor
 String MQ5  ;                                   // This defines the analog PIN A1 , which is connected to MQ5 gas sensor
 String Temp ;                                   // This defines the analog PIN D4 , which is connected to temperature sensor
 String Hum  ;                                   // This defines the analog PIN D4 , which is connected to humidity sensor
-int MQ3_ADC_VALUE;
-int MQ5_ADC_VALUE ;
-int Temp_ADC_VALUE ;
-int Hum_ADC_VALUE ;
-String damageReading ;
+int MQ3_CO_PPM ;
+int MQ5_LPG_PPM ;
+int Temp_VALUE ;
+int Hum_VALUE ;
+String damageReading ;                          // used to read the first character in the serial which is just for marking 
+                                                // start of information
 
 /**********************RGB pin setup***************************************/
 int redPin_LPG = 9;
@@ -33,89 +38,105 @@ int redPin_CO = 3;
 int greenPin_CO = 5;
 int bluePin_CO = 6;
 
+
 /******************Program Setup *************************************************/
 void setup() {
   
-  startTime = millis();
+  startTime = millis();    // used to ensure that data is posted in interval of 5 seconds or more
   Serial.begin(38400);
   
-  SoftSer.begin(38400);
+  Esp8266_Serial.begin(38400);
+
+  // if using the module for the first time uncomment this otherwise the esp8266 module will remeber the previous connection .
+    //  while(1){  // iterate until the wifi is connected 
+    //  connectEsp8266WIFI();
+    //  if(Esp8266_Serial.find("WIFI CONNECTED\r\n")==1){
+    //     break ;
+    //   }
+    //  }
   
   Serial.println("CLEARSHEET"); // clears EXCEL sheet starting at row 1
   Serial.println("CLEARDATA");
-  Serial.println("LABEL,Date,Time,LPG,CO,TEMP,HUM");
+  Serial.println("LABEL,Date,Time,LPG,CO,TEMP,HUM"); // labels on excel spreadsheet
 
 }
 
 void loop() {
-  
-
-   //Serial.println( (String) "DATA,DATE,TIME," + findPPM_LPG_MQ5() + "," + findPPM_CO_MQ3() + ",AUTOSCROLL_20" );
    
-         if( Serial.available() > 0 ){
-            
-            damageReading = Serial.readStringUntil( '<');                 
-             MQ3 = Serial.readStringUntil(',');
-             MQ5 = Serial.readStringUntil(',');
-            Temp = Serial.readStringUntil(',');
-            Hum = Serial.readStringUntil('>');
+ if( Serial.available() > 0 ){
+
+          //Read the values in the serial buffer
+          damageReading = Serial.readStringUntil( '<');                 
+          MQ3 = Serial.readStringUntil(',');
+          MQ5 = Serial.readStringUntil(',');
+          Temp = Serial.readStringUntil(',');
+          Hum = Serial.readStringUntil('>');
                   
-             MQ3_ADC_VALUE = MQ3.toInt();
-             MQ5_ADC_VALUE = MQ5.toInt();
-             Temp_ADC_VALUE = Temp.toInt();
-             Hum_ADC_VALUE =  Hum.toInt();
+          //convert the strings to integers
+          MQ3_CO_PPM   = MQ3.toInt();
+          MQ5_LPG_PPM  = MQ5.toInt();
+          Temp_VALUE   = Temp.toInt();
+          Hum_VALUE    =  Hum.toInt();
+         
+          setAppropriateRGBColours() ;
+       
+          // write the values to excelyufxjgxy
+          Serial.println( (String) "DATA,DATE,TIME," +MQ3_CO_PPM + "," + MQ5_LPG_PPM  +  "," + Temp_VALUE + "," + Hum_VALUE +  ",AUTOSCROLL_20" );
 
-          waitTime = millis()-startTime; 
+   
+           waitTime = millis()-startTime; 
+           // post the values to thingspeak IoT web server
+           if(waitTime > timeIntervalBetweenWrite ) {
+                 postDataToThingSpeak(MQ3_CO_PPM  ,  MQ5_LPG_PPM  , Temp_VALUE, Hum_VALUE );
+                 startTime = millis();
+             }
+          
 
-          if(waitTime > timeIntervalBetweenWrite ) {
-             postDataToThingSpeak(MQ3_ADC_VALUE , MQ5_ADC_VALUE  , Temp_ADC_VALUE, Hum_ADC_VALUE );
-             startTime = millis();
-         }
+     }
 
-//Threshold processes for air quality color system 
-if( MQ3_ADC_VALUE> 100){
+}
+
+void setAppropriateRGBColours(){
   
-  setColor(0, 255, 255);  //red, Dangerous air quality 
+   //Threshold processes for air quality color system 
+        if( MQ3_CO_PPM > 100){
+  
+            setColor(0, 255, 255);  //red, Dangerous air quality 
  
-  }else if(MQ3_ADC_VALUE< 100  &&  MQ3_ADC_VALUE> 70 ){
+        }else if(MQ3_CO_PPM< 100  &&  MQ3_CO_PPM> 70 ){
     
-   setColor(5,215, 255);  //yellow, Moderate air quality
+            setColor(5,215, 255);  //yellow, Moderate air quality
    
-    }else if(MQ3_ADC_VALUE< 70  &&  MQ3_ADC_VALUE> 40){
+        }else if(MQ3_CO_PPM< 70  &&  MQ3_CO_PPM> 40){
     
-   setColor(0, 0, 255);  //yellow, Moderate air quality
+            setColor(0, 0, 255);  //yellow, Moderate air quality
    
-  }else{
+        }else{
       
-        setColor(255,0,255);  //green, Good air quality
-      }
+            setColor(255,0,255);  //green, Good air quality
+        }
 
              
 
-  if( MQ5_ADC_VALUE> 400){
+        if( MQ5_LPG_PPM > 400){
   
-  setColor2(0, 255, 255);  //red, Dangerous air quality 
+            setColor2(0, 255, 255);  //red, Dangerous air quality 
  
-  }else if(MQ5_ADC_VALUE< 400  &&  MQ5_ADC_VALUE> 300){
+         }else if(MQ5_LPG_PPM < 400  &&  MQ5_LPG_PPM > 300){
     
-   setColor2(5,215,255);  //ORANGE, Moderate air quality
+            setColor2(5,215,255);  //ORANGE, Moderate air quality
    
-  }else if(MQ5_ADC_VALUE< 300  &&  MQ5_ADC_VALUE> 200){
+         }else if(MQ5_LPG_PPM < 300  &&  MQ5_LPG_PPM > 200){
     
-   setColor2(0, 0, 255);  //yellow, Moderate air quality
+            setColor2(0, 0, 255);  //yellow, Moderate air quality
    
-  }else{
+         }else{
       
-        setColor2(255,0,255);//green, Good air quality
+             setColor2(255,0,255);//green, Good air quality
         
-      }
-
-}
-   Serial.println( (String) "DATA,DATE,TIME," + MQ3_ADC_VALUE+ "," + MQ5_ADC_VALUE +  "," + Temp_ADC_VALUE + "," + Hum_ADC_VALUE +  ",AUTOSCROLL_20" );
-
-}
-
-
+         }
+  
+  }
 
 
 
@@ -136,29 +157,19 @@ void setColor(int red, int green, int blue)
 
 void postDataToThingSpeak(int data1 ,int data2 ,int data3 ,int data4 ){
 
-  /*  String join="AT+CWJAP=\""; // Join accesspoint
-  join+=SSID;
-  join+="\",\"";
-  join+=PASS;
-  join+="\"";
-  SoftSer.println(join);
-  delay(5000);*/
-
-
   // TCP connection
   String cmd = "AT+CIPSTART=\"TCP\",\"";
   cmd += "184.106.153.149"; // api.thingspeak.com
   cmd += "\",80";
-  SoftSer.println(cmd);
+  Esp8266_Serial.println(cmd);
 
-  if (SoftSer.find("Error")) 
+  if (Esp8266_Serial.find("Error")) 
   {
-    //Serial.println("AT+CIPSTART error");
     return;
   }
   
   
-  // put together the get string command
+  // put together the GET string command
   String getStr = "GET /update?api_key=";
   getStr += apiKey;
   getStr += "&field1=";
@@ -172,31 +183,42 @@ void postDataToThingSpeak(int data1 ,int data2 ,int data3 ,int data4 ){
   getStr += "\r\n\r\n";
 
 
- 
+  // send the length of the data
   cmd = "AT+CIPSEND=";
   cmd += String(getStr.length());
-  SoftSer.println(cmd);
+  Esp8266_Serial.println(cmd);
 
-  if (SoftSer.find(">")) 
+  if (Esp8266_Serial.find(">")) 
   {
-    SoftSer.print(getStr); // Send data.
+    Esp8266_Serial.print(getStr); // Send data.
   }
   else 
   {
     Reset() ;
-    SoftSer.println("AT+CIPCLOSE");
+    Esp8266_Serial.println("AT+CIPCLOSE");
     
     Serial.println("AT+CIPCLOSE"); // If this shows on the serial monitor the data did not send.
   }
-  // The AT+RST command resets the ESP8266, I'm doing this because data is uploaded much more reliably. 
-  SoftSer.println("AT+RST"); 
+  // Resert the esp8266 for reliable data transfer. 
+  Esp8266_Serial.println("AT+RST"); 
   
+ 
   
+}
+
+void connectEsp8266WIFI(){
+    String join="AT+CWJAP=\""; // Join accesspoint
+  join+=SSID;
+  join+="\",\"";
+  join+=PASS;
+  join+="\"";
+  Esp8266_Serial.println(join);
+  delay(5000);
   
 }
 
 void Reset(){
   startTime = millis();
   
-  SoftSer.begin(38400);
+  Esp8266_Serial.begin(38400);
 }
